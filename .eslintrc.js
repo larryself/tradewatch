ac7dcf2dd60595b90b83ede3ad369e17fec0ce75
Yan Musikhin <yanmusikhin@gmail.com>
2023-06-20 11:40:40 +0400

    docs: add lint, readme and libs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,106 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-};
+
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+
+  plugins: [
+    '@typescript-eslint',
+    'import',
+    'promise',
+    'prettier',
+    'simple-import-sort',
+  ],
+
+  extends: [
+    '@react-native-community',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:promise/recommended',
+  ],
+
+  rules: {
+    quotes: [
+      'error',
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true,
+      },
+    ],
+    'react/jsx-no-literals': 'off',
+    'padding-line-between-statements': [
+      'error',
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'return',
+      },
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'case',
+      },
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'default',
+      },
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'block-like',
+      },
+    ],
+    'prefer-const': ['error'],
+    'simple-import-sort/imports': [
+      'warn',
+      {
+        groups: [['^\\u0000'], ['^react', '^[^.]'], ['^src/'], ['^\\.']],
+      },
+    ],
+
+    'max-lines': ['error', 500],
+    'no-console': 'error',
+    'object-shorthand': 'error',
+    'no-unneeded-ternary': 'error',
+    'no-nested-ternary': 'error',
+    'newline-before-return': 'warn',
+    semi: ['error', 'never'],
+
+    // prettier
+    'prettier/prettier': [
+      'error',
+      {
+        tabWidth: 2,
+        trailingComma: 'all',
+        semi: false,
+        arrowParens: 'always',
+        singleQuote: true,
+        printWidth: 80,
+        bracketSpacing: true,
+      },
+    ],
+
+    'import/newline-after-import': 'error',
+
+    // react
+    'react/react-in-jsx-scope': 'off',
+
+    // promise
+    'promise/prefer-await-to-then': 'warn',
+    'promise/prefer-await-to-callbacks': 'warn',
+
+    'no-shadow': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+
+    'react-native/no-unused-styles': 'error',
+    'react-native/split-platform-components': 'warn',
+    'react-native/no-single-element-style-arrays': 'error',
+    'react/sort-comp': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+}
